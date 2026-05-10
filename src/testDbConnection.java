8cf09ac053ad6607112dcdb8c5f161bd0f90ba78
MidnightStellarrr <hannahgranada458@gmail.com>
2026-05-10 14:43:31 +0800

    db connection to  java

---
+++ src/testDbConnection.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class App  
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/bookflow_db", 
+                "root",      
+                ""           
+            );
+            
+            Statement stmt = con.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+            
+            while(rs.next())
+            {
+                // Get actual data from each column
+                System.out.println(rs.getString("username"));  
+            }           
+            con.close();
+        }
+        catch (Exception e)  
+        {
+            System.out.println(e);
+        }
+    }
+}
